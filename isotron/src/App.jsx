67c91ff5156383b546f73c26cce58900899a0c53
Daniel <sandsdalen.daniel@gmail.com>
2025-02-26 15:27:13 +0100

    Endret på frontend

--- isotron/src/App.jsx
+++ isotron/src/App.jsx
@@ -1,12 +1,16 @@
+import './App.css';
 import Header from './components/header';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
-      <main className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Velkommen til Appen!</h1>
-        <p className="text-gray-700">Her er innholdet ditt.</p>
+      <main className="p-4 mt-16 flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold mb-4">Velkommen til Isotron!</h1>
+        <p className="text-gray-700 text-center">
+          Denne siden assisterer personer i hvordan håndtere data, filer, passord og mer i henhold til det nye ISO27001-direktivet fra EU. 
+          Vi tilbyr veiledning og ressurser for å sikre at din organisasjon oppfyller de nyeste sikkerhetsstandardene.
+        </p>
       </main>
     </div>
   );
--- isotron/src/components/header.jsx
+++ isotron/src/components/header.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import BugIcon from './svg/bug.svg?react';
+import IsotronLogo from './svg/isotron.svg?react';
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,8 +14,8 @@ function Header() {
       <nav className="mx-auto flex max-w-7xl items-center justify-between">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img className="h-8 w-auto" src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Logo" />
+            <span className="sr-only">Isotron</span>
+            <IsotronLogo className="h-8 w-auto text-white" />
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -26,8 +28,8 @@ function Header() {
         </div>
         <div className={`hidden lg:flex lg:gap-x-12 ${isMenuOpen ? 'block' : 'hidden'}`}>
           <div className="relative">
-            <button type="button" className="flex items-center gap-x-1 text-sm font-semibold text-gray-900" aria-expanded={isMenuOpen} onClick={toggleMenu}>
-              Product
+            <button type="button" className="flex items-center gap-x-1 text-sm font-semibold text-white" aria-expanded={isMenuOpen} onClick={toggleMenu}>
+              ISO27001
               <svg className="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
               </svg>
@@ -91,29 +93,27 @@ function Header() {
                     <p className="mt-1 text-gray-600">Build strategic funnels that will convert</p>
                   </div>
                 </div>
-              </div>
-              <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                <a href="#" className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold text-gray-900 hover:bg-gray-100">
-                  <svg className="h-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fillRule="evenodd" d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm6.39-2.908a.75.75 0 0 1 .766.027l3.5 2.25a.75.75 0 0 1 0 1.262l-3.5 2.25A.75.75 0 0 1 8 12.25v-4.5a.75.75 0 0 1 .39-.658Z" clipRule="evenodd" />
-                  </svg>
-                  Watch demo
-                </a>
-                <a href="#" className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold text-gray-900 hover:bg-gray-100">
-                  <svg className="h-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fillRule="evenodd" d="M2 3.5A1.5 1.5 0 0 1 3.5 2h1.148a1.5 1.5 0 0 1 1.465 1.175l.716 3.223a1.5 1.5 0 0 1-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 0 0 6.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 0 1 1.767-1.052l3.223.716A1.5 1.5 0 0 1 18 15.352V16.5a1.5 1.5 0 0 1-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 0 1 2.43 8.326 13.019 13.019 0 0 1 2 5V3.5Z" clipRule="evenodd" />
-                  </svg>
-                  Contact sales
-                </a>
+                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                  <a href="#" className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold text-gray-900 hover:bg-gray-100">
+                    <svg className="h-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm6.39-2.908a.75.75 0 0 1 .766.027l3.5 2.25a.75.75 0 0 1 0 1.262l-3.5 2.25A.75.75 0 0 1 8 12.25v-4.5a.75.75 0 0 1 .39-.658Z" clipRule="evenodd" />
+                    </svg>
+                    Watch demo
+                  </a>
+                  <a href="#" className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold text-gray-900 hover:bg-gray-100">
+                    <BugIcon className="h-5 w-5 flex-none text-gray-400" />
+                    Melde feil på siden
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-          <a href="#" className="text-sm font-semibold text-gray-900">Features</a>
-          <a href="#" className="text-sm font-semibold text-gray-900">Marketplace</a>
-          <a href="#" className="text-sm font-semibold text-gray-900">Company</a>
+          <a href="#" className="text-sm font-semibold text-white">ISO27001</a>
+          <a href="#" className="text-sm font-semibold text-white">Opplæring</a>
+          <a href="#" className="text-sm font-semibold text-white">Om</a>
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+          <a href="#" className="text-sm font-semibold text-white">Søk <span aria-hidden="true">&rarr;</span></a>
         </div>
       </nav>
     </header>
